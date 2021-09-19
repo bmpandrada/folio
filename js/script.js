a6aee9d4d27e591a46254ab2b38ffd6d6d545e5b
Bruce Andrada <88586952+bmpandrada@users.noreply.github.com>
2021-09-19 23:00:13 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
         $("#about img").css({
-            width: (80 + scroll / 20)
+            width: (100 + scroll / 20)
 
         });
 
