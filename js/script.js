09810ababf4788f0c465299c6c8a0f3208a8cc91
Bruce Andrada <88586952+bmpandrada@users.noreply.github.com>
2021-09-22 20:52:40 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
 
-    
+
+
     //THIS IS FOR link smooth behavior
-
     $('a').click(function() {
-
         $('html, body').animate({
             scrollTop: $($(this).attr('href')).offset().top
         }, 1000);
@@ -13,19 +12,26 @@ $(document).ready(function() {
 
     //navigation switch sticky
     $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-        $("#about img").css({
-            width: (150 + scroll / 17)
 
-        });
+
+
+
+
+
 
         if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
         } else {
             $('.navbar').removeClass('sticky');
         }
-    });
 
+        if (this.scrollY > 400) {
+            $('.about .about-content .left img,  .about .about-content .right').addClass("sticky");
+
+        } else {
+            $('.about .about-content .left img, .about .about-content .right').removeClass('sticky');
+        }
+    });
 
     //THIS IS FOR MENU TOGGLE
     //MENU TAB WILL APPEAR AFTER MINIMIZING
