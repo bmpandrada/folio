58775e65c8c7155d3c163a32e84ccef7b9d6b2c1
Bruce Andrada <88586952+bmpandrada@users.noreply.github.com>
2021-09-27 22:34:39 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,43 +1,29 @@
 $(document).ready(function() {
-
-
-
-
     //THIS IS FOR link smooth behavior
     $('a').click(function() {
         $('html, body').animate({
             scrollTop: $($(this).attr('href')).offset().top
         }, 1000);
     });
-
-
     //navigation switch sticky
     $(window).scroll(function() {
-
-      
-
-
         //-------------------------///
         if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
         } else {
             $('.navbar').removeClass('sticky');
         }
-
         if (this.scrollY > 400) {
             $('.about .about-content .left img,  .about .about-content .right').addClass("sticky");
-
         } else {
             $('.about .about-content .left img, .about .about-content .right').removeClass('sticky');
         }
-          if (this.scrollY > 720) {
+        if (this.scrollY > 720) {
             $('.service .title, .service .service-content .card').addClass("sticky");
         } else {
             $('.service .title, .service .service-content .card').removeClass('sticky');
         }
-
     });
-
     //THIS IS FOR MENU TOGGLE
     //MENU TAB WILL APPEAR AFTER MINIMIZING
     $('.menu-btn').click(function() {
@@ -45,15 +31,20 @@ $(document).ready(function() {
 
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn img').toggleClass("active");
+
     });
-
-
     //THIS IS FOR AFTER CLICK MENU//
     $('.navbar .menu').on('click', event => {
         $(event.currentTarget).toggleClass("active");
         $(this).find('.menu-btn').toggleClass('active');
 
-    });
 
+    });
+    $('.about .about-content .right .read').click(function() {
+        $(this).toggleClass('active');
+        $(".about .about-content .right .read a").text('See less');
+        $('.about .about-content .right .more').delay(200).toggle(1000)("active");
+
+    });
 
 });
